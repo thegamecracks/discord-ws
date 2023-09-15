4bdf938ed240f23d657dd108f883c5495f391f7d
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-09-15 09:17:27 -0400

    feat: add CLI arguments for connection customization

--- src/discord_ws/__main__.py
+++ src/discord_ws/__main__.py
@@ -1,10 +1,59 @@
+"""A simple command-line interface for connecting to the Discord gateway."""
+import argparse
 import asyncio
+import getpass
 import logging
 import os
 
 from . import Client, Intents
 
-token = os.environ["TOKEN"]
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument(
+    "--env-token",
+    help="Read environment variable instead of prompting for token",
+)
+intents_group = parser.add_mutually_exclusive_group(required=True)
+intents_group.add_argument(
+    "--no-intents",
+    action="store_const",
+    const=Intents.none(),
+    dest="intents",
+    help="Do not enable any intents",
+)
+intents_group.add_argument(
+    "--standard-intents",
+    action="store_const",
+    const=Intents.standard(),
+    dest="intents",
+    help="Enable standard intents",
+)
+intents_group.add_argument(
+    "--all-intents",
+    action="store_const",
+    const=Intents.all(),
+    dest="intents",
+    help="Enable standard and privileged intents",
+)
+intents_group.add_argument(
+    "--intents",
+    help="The intents value to use as an integer",
+    type=lambda s: Intents(int(s)),
+)
+parser.add_argument(
+    "--zlib-stream",
+    action="store_const",
+    const="zlib-stream",
+    dest="compression",
+    help="Use zlib transport compression",
+)
+
+args = parser.parse_args()
+
+if args.env_token is not None:
+    token = os.environ[args.env_token]
+else:
+    token = getpass.getpass("Token: ")
 
 
 async def main():
@@ -13,7 +62,8 @@ async def main():
 
     client = await Client.create(
         token=token,
-        intents=Intents.all(),
+        intents=args.intents,
+        compress=args.compression is not None,
     )
 
     await client.run()
