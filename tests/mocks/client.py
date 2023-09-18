bce4696e601ddf66c2f1ef9740084f6a35321b13
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-09-17 21:17:57 -0400

    test: rewrite client fixtures to be optional

--- tests/mocks/client.py
+++ tests/mocks/client.py
@@ -9,42 +9,45 @@ from .websockets import create_websocket
 
 
 @pytest.fixture
-def client_dispatch() -> Mock:
-    return Mock()
-
-
-@pytest.fixture
-def client_stream() -> AsyncMock:
-    return AsyncMock(spec_set=Stream)
-
-
-@pytest.fixture
-def client_ws() -> AsyncMock:
-    return create_websocket()
-
-
-@pytest.fixture
-def client(
-    client_dispatch,
-    client_stream,
-    client_ws,
-    monkeypatch: pytest.MonkeyPatch,
-) -> Client:
-    def get_client_stream(client, *args, **kwargs):
-        return client_stream
-
-    def get_client_ws(*args, **kwargs):
-        return client_ws
-
+def client() -> Client:
     client = Client(
         gateway_url="wss://example.invalid",
         token="Bot TOKEN",
         intents=Intents.all(),
-        on_dispatch=client_dispatch,
+        on_dispatch=lambda event: None,
     )
+    return client
 
-    monkeypatch.setattr(websockets.client, "connect", get_client_ws, raising=False)
+
+@pytest.fixture
+def client_dispatch(client) -> Mock:
+    mock = Mock()
+    client.on_dispatch = mock
+    return mock
+
+
+@pytest.fixture
+def client_stream(
+    client,
+    monkeypatch: pytest.MonkeyPatch,
+) -> AsyncMock:
+    def get_client_stream(client, *args, **kwargs):
+        return mock
+
+    mock = AsyncMock(spec_set=Stream)
     monkeypatch.setattr(PlainTextStream, "__new__", get_client_stream)
     monkeypatch.setattr(ZLibStream, "__new__", get_client_stream)
+    return mock
 
-    return client
+
+@pytest.fixture
+def client_ws(
+    client,
+    monkeypatch: pytest.MonkeyPatch,
+) -> AsyncMock:
+    def get_client_ws(*args, **kwargs):
+        return ws
+
+    ws = create_websocket()
+    monkeypatch.setattr(websockets.client, "connect", get_client_ws, raising=False)
+    return ws
