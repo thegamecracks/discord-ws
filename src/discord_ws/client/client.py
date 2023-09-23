thegamecracks/discord-ws

commit ac0abfa24088719a93bf00d879cbfb7b76204372

refactor!: use more exceptions to interrupt connections
Consolidates all ws.close() calls into client for simplicity,
and improves granularity of errors for reconnect=False users

src/discord_ws/client/client.py
@@ -23,8 +23,11 @@ from discord_ws import constants
 from discord_ws.errors import (
     AuthenticationFailedError,
     ConnectionClosedError,
+    GatewayInterrupt,
+    GatewayReconnect,
     HeartbeatLostError,
     PrivilegedIntentsError,
+    SessionInvalidated,
 )
 from discord_ws.http import _create_user_agent
 from discord_ws.intents import Intents
@@ -201,6 +204,15 @@ class Client:
         :param reconnect:
             If True, this method will reconnect to Discord
             where possible.
+        :raises GatewayReconnect:
+            The gateway has asked us to reconnect.
+            Only raised when reconnect is False.
+        :raises HeartbeatLostError:
+            The gateway failed to acknowledge our heartbeat.
+            Only raised when reconnect is False.
+        :raises SessionInvalidated:
+            The gateway has invalidated our session.
+            Only raised when reconnect is False.
 
         """
         if self.gateway_url is None:
@@ -225,7 +237,7 @@ class Client:
                     await self._run_forever(session_id=session_id)
             except ConnectionClosed as e:
                 reconnect = self._handle_connection_closed(e) and reconnect
-            except HeartbeatLostError as e:
+            except GatewayInterrupt:
                 if not reconnect:
                     raise
 
@@ -287,17 +299,29 @@ class Client:
         return self._resume_gateway_url is not None and self._session_id is not None
 
     async def _run_forever(self, *, session_id: str | None) -> None:
-        async with (
-            self._create_stream(),
-            self._heart.stay_alive(),
-        ):
-            if session_id is None:
-                await self._identify()
-            else:
-                await self._resume(session_id)
+        try:
+            async with (
+                self._create_stream(),
+                self._heart.stay_alive(),
+            ):
+                if session_id is None:
+                    await self._identify()
+                else:
+                    await self._resume(session_id)
 
-            while True:
-                await self._receive_event()
+                while True:
+                    await self._receive_event()
+        except GatewayReconnect:
+            await self._ws.close(1002, reason="Reconnect ACK")
+            raise
+        except HeartbeatLostError:
+            await self._ws.close(1002, reason="Heartbeat ACK lost")
+            raise
+        except SessionInvalidated as e:
+            if not e.resumable:
+                self._invalidate_session()
+            await self._ws.close(1002, reason="Invalid Session ACK")
+            raise
 
     def _add_gateway_params(self, url: str) -> str:
         """Adds query parameters to the given gateway URL.
@@ -436,15 +460,13 @@ class Client:
         elif event["op"] == 7:
             # Reconnect
             log.debug("Received request to reconnect")
-            await self._ws.close(1002, reason="Reconnect ACK")
+            raise GatewayReconnect()
 
         elif event["op"] == 9:
             # Invalid Session
             event = cast(InvalidSession, event)
             log.debug("Session has been invalidated")
-            if not event["d"]:
-                self._invalidate_session()
-            await self._ws.close(1002, reason="Invalid Session ACK")
+            raise SessionInvalidated(resumable=event["d"])
 
         elif event["op"] == 10:
             # Hello

src/discord_ws/__init__.py
@@ -3,8 +3,11 @@ from .errors import (
     AuthenticationFailedError,
     ClientError,
     ConnectionClosedError,
+    GatewayInterrupt,
+    GatewayReconnect,
     HeartbeatLostError,
     PrivilegedIntentsError,
+    SessionInvalidated,
 )
 from .intents import Intents
 

src/discord_ws/client/heartbeat.py
@@ -138,8 +138,7 @@ class Heart:
     async def _send_heartbeat(self) -> None:
         """Sends a heartbeat payload to Discord."""
         if not self.acknowledged:
-            log.debug("Heartbeat not acknowledged, closing connection")
-            await self.client._ws.close(1002, reason="Heartbeat ACK lost")
+            log.debug("Heartbeat not acknowledged")
             raise HeartbeatLostError()
 
         assert self.client._stream is not None

src/discord_ws/errors.py
@@ -51,6 +51,43 @@ class PrivilegedIntentsError(ConnectionClosedError):
         ).format(intents)
 
 
+class GatewayInterrupt(ClientError):
+    """
+    The client has been asked by the gateway to interrupt the current connection.
+
+    This exception and its subclasses are expected to be raised during normal
+    operation, and should not impact the client's ability to reconnect to the
+    gateway.
+
+    """
+
+
+class GatewayReconnect(GatewayInterrupt):
+    """The client has been asked to reconnect to the gateway.
+
+    This corresponds with the Reconnect (9) opcode.
+
+    """
+
+    def __init__(self) -> None:
+        super().__init__("Discord has requested our client to reconnect.")
+
+
+class SessionInvalidated(GatewayInterrupt):
+    """The client's session has been invalidated by the gateway.
+
+    This corresponds with the Invalid Session (9) opcode.
+
+    """
+
+    resumable: bool
+    """Indicates if the session can be resumed."""
+
+    def __init__(self, resumable: bool) -> None:
+        super().__init__("Discord has invalidated our session.")
+        self.resumable = resumable
+
+
 class HeartbeatLostError(ClientError):
     """A heartbeat acknowledgement from the server was missed."""
 

commit 919c737b23c28bce14e9708b6e5c2493802e5014

fix: allow reconnecting when client loses heartbeat
Regression caused by ac0abfa24088719a93bf00d879cbfb7b76204372

src/discord_ws/client/client.py
@@ -245,7 +245,7 @@ class Client:
                 e = _unwrap_first_exception(eg)
                 assert e is not None
                 reconnect = self._handle_connection_closed(e) and reconnect
-            except* GatewayInterrupt:
+            except* (GatewayInterrupt, HeartbeatLostError):
                 if not reconnect:
                     raise
 