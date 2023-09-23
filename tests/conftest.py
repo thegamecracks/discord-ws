aa180e386f7d422182159250f2c45cb736b94cd9
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-09-23 13:10:19 -0400

    test: capture debug logs across all tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,11 @@
+import logging
+
 import pytest
+
+
+@pytest.fixture(autouse=True)
+def enable_logging(caplog: pytest.LogCaptureFixture):
+    caplog.set_level(logging.DEBUG, "discord_ws")
 
 
 @pytest.fixture(autouse=True)
