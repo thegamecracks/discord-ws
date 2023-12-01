407b5d0a4976ff7a721a3e09e97a8a606311b673
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-12-01 10:00:13 -0500

    feat: use __name__ == "__main__" in __main__.py
    Required by Sphinx
    https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html

--- src/discord_ws/__main__.py
+++ src/discord_ws/__main__.py
@@ -8,55 +8,59 @@ import os
 from . import Client, Intents
 
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument(
-    "--env-token",
-    help="Read environment variable instead of prompting for token",
-)
-intents_group = parser.add_mutually_exclusive_group(required=True)
-intents_group.add_argument(
-    "--no-intents",
-    action="store_const",
-    const=Intents.none(),
-    dest="intents",
-    help="Do not enable any intents",
-)
-intents_group.add_argument(
-    "--standard-intents",
-    action="store_const",
-    const=Intents.standard(),
-    dest="intents",
-    help="Enable standard intents",
-)
-intents_group.add_argument(
-    "--all-intents",
-    action="store_const",
-    const=Intents.all(),
-    dest="intents",
-    help="Enable standard and privileged intents",
-)
-intents_group.add_argument(
-    "--intents",
-    help="The intents value to use as an integer",
-    type=lambda s: Intents(int(s)),
-)
-parser.add_argument(
-    "--zlib-stream",
-    action="store_const",
-    const="zlib-stream",
-    dest="compression",
-    help="Use zlib transport compression",
-)
-
-args = parser.parse_args()
-
-if args.env_token is not None:
-    token = os.environ[args.env_token]
-else:
-    token = getpass.getpass("Token: ")
-
-
 async def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--env-token",
+        help="Read environment variable instead of prompting for token",
+    )
+    intents_group = parser.add_mutually_exclusive_group(required=True)
+    intents_group.add_argument(
+        "--no-intents",
+        action="store_const",
+        const=Intents.none(),
+        dest="intents",
+        help="Do not enable any intents",
+    )
+    intents_group.add_argument(
+        "--standard-intents",
+        action="store_const",
+        const=Intents.standard(),
+        dest="intents",
+        help="Enable standard intents",
+    )
+    intents_group.add_argument(
+        "--all-intents",
+        action="store_const",
+        const=Intents.all(),
+        dest="intents",
+        help="Enable standard and privileged intents",
+    )
+    intents_group.add_argument(
+        "--intents",
+        help="The intents value to use as an integer",
+        type=lambda s: Intents(int(s)),
+    )
+    parser.add_argument(
+        "--zlib-stream",
+        action="store_const",
+        const="zlib-stream",
+        dest="compression",
+        help="Use zlib transport compression",
+    )
+
+    args = parser.parse_args()
+
+    if args.env_token is not None:
+        token = os.environ[args.env_token]
+    else:
+        token = getpass.getpass("Token: ")
+
+    logging.basicConfig(
+        format="%(name)30s (%(levelname)8s) => %(message)s",
+    )
+    logging.getLogger(__package__).setLevel(logging.DEBUG)
+
     client = Client(
         token=f"Bot {token}",
         intents=args.intents,
@@ -65,12 +69,8 @@ async def main():
 
     await client.run()
 
-logging.basicConfig(
-    format="%(name)30s (%(levelname)8s) => %(message)s",
-)
-logging.getLogger(__package__).setLevel(logging.DEBUG)
-
-try:
-    asyncio.run(main())
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
